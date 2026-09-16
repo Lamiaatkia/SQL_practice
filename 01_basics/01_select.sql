91fdc9b13dbaba41bfeed6889d9f3c3c59fb0cfb
ATKIA JAHAN LAMIA <atkialamia12@gmail.com>
2026-09-16 20:01:09 +0900

    where filtering

--- 01_basics/01_select.sql
+++ 01_basics/01_select.sql
@@ -1,8 +1,10 @@
 -- SQL Practice 01
 -- SELECT
-
 SELECT * FROM students;
-
 SELECT name FROM students;
-
 SELECT name, age FROM students;
+
+
+--where practice
+select* from customer
+where country = 'germany';
